bb13e29010bdb3debccb149e0b147716ad5e0f0a
ArturiosBoss <maltcev-artur@yandex.ru>
2021-07-20 23:23:54 +0300

    Lession_2

--- project/src/main/resources/db/migration/V1__initialize.sql
+++ project/src/main/resources/db/migration/V1__initialize.sql
@@ -53,37 +53,4 @@ VALUES
 (1, 2),
 (1, 3);
 
---DROP TABLE IF EXISTS students;
---
---CREATE TABLE students (
---	id int(11) NOT NULL AUTO_INCREMENT,
---    name VARCHAR(100) DEFAULT NULL,
---    PRIMARY KEY(id)
---) ENGINE=InnoDB CHARSET=utf8;
---
---DROP TABLE IF EXISTS courses;
-
---CREATE TABLE courses (
---	id int(11) NOT NULL AUTO_INCREMENT,
---    title VARCHAR(100) DEFAULT NULL,
---    PRIMARY KEY(id)
---) ENGINE=InnoDB CHARSET=utf8;
---
---DROP TABLE IF EXISTS students_courses;
---
---CREATE TABLE students_courses (
---	student_id int(11) NOT NULL,
---    course_id int(11) NOT NULL,
---
---    PRIMARY KEY (student_id, course_id),
---
---    CONSTRAINT FK_STUDENT FOREIGN KEY (student_id)
---    REFERENCES students (id)
---    ON DELETE NO ACTION ON UPDATE NO ACTION,
---
---    CONSTRAINT FK_COURSE FOREIGN KEY (course_id)
---    REFERENCES courses (id)
---    ON DELETE NO ACTION ON UPDATE NO ACTION
---) ENGINE=InnoDB CHARSET=utf8;
-
 SET FOREIGN_KEY_CHECKS = 1;
